a1347a377ae95d7df462a48405817d45b83d8e84
dzvinka <yarish@ucu.edu.ua>
2020-10-07 13:26:32 +0300

    added exchange history logging

--- reproduce_results.py
+++ reproduce_results.py
@@ -12,26 +12,28 @@ from read_datasets import get_emnist_letters
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle as shuffle_dataset
 
+
 import wandb
 from wandb.keras import WandbCallback
 
 
 wandb.init(
   project="deep-tempering",
-  name="test-cifar10-const-dropout-lr-with-coeff",
+  name="test-cifar10-lr-log-exchange",
   notes="",
   config={
     "model_name": "lenet5",
     "dataset_name": "cifar10",
     "model_builder": "lenet5_cifar10_builder",
-    "n_replicas": 8,
+    "n_replicas": 2,
     "swap_step": 800,
     "burn_in": 25000,
     "batch_size": 128,
     "epochs": 400,
+    "proba_coeff": 300000,
     "train_data_size": 45000,
     "lr_range": [0.015, 0.01],
-    "dropout_range": [0.6, 0.6]  #ToDo: if value of hp==0 we get ZeroDivision error so if we need const dropout value != 0, we specify it
+    "dropout_range": [0.999, 0.999]  #ToDo: if value of hp==0 we get ZeroDivision error so if we need const dropout value != 0, we specify it
 }
 )
 
@@ -43,7 +45,7 @@ model_builders = {"lenet5_cifar10_builder": lenet5_cifar10_builder, 'lenet5_emni
 
 hp = {1: {'learning_rate': [0.1 for _ in range(config.n_replicas)],
         'dropout_rate': np.linspace(config.dropout_range[0], config.dropout_range[1], config.n_replicas),},
-       25000: {'learning_rate': np.linspace(config.lr_range[0], config.lr_range[1], config.n_replicas),
+       71: {'learning_rate': np.linspace(config.lr_range[0], config.lr_range[1], config.n_replicas),
         'dropout_rate': np.linspace(config.dropout_range[0], config.dropout_range[1], config.n_replicas),}
        }
 
@@ -84,13 +86,18 @@ model = dt.EnsembleModel(model_builders[config.model_builder])
 model_noswap = dt.EnsembleModel(model_builders[config.model_builder])
 
 
+class SGDLearningRateTracker(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        lrs = [tf.keras.backend.eval(self.model._train_attrs[i]['optimizer'].lr * (1. / (1. + self.model._train_attrs[i]['optimizer'].decay *
+                                                                                         tf.cast(self.model._train_attrs[i]['optimizer'].iterations, dtype=tf.float32)))) for i in range(len(self.model._train_attrs))]
+        print(lrs)
+
 model.compile(optimizer=tf.keras.optimizers.SGD(),
               loss='categorical_crossentropy',
               metrics=['accuracy'],
               n_replicas=config.n_replicas)
 
-print(model._train_attrs[0]['model'].summary())
-
+# model.summary()
 
 
 history = model.fit(x_train,
@@ -100,16 +107,38 @@ history = model.fit(x_train,
                     batch_size=config.batch_size,
                     epochs=config.epochs,
                     swap_step=config.swap_step,
-                    burn_in=config.burn_in,)
-                    # callbacks=[WandbCallback(data_type="image",)]) wandbcallback doesnt work with EnsembleModel, it lacks several attributes
+                    burn_in=config.burn_in,
+                    # coeff=config.proba_coeff
+                    # callbacks=[WandbCallback(data_type="image",)]) #wandbcallback doesnt work with EnsembleModel, it lacks several attributes
+                    # callbacks=[SGDLearningRateTracker(),
+                    #            tf.keras.callbacks.TensorBoard(log_dir='./logs/test/', write_graph=False, update_freq='batch'),
+                    #            ]
+                    )
 
 
 
 
-
+ex_history = history.exchange_history
 history = history.history
+
+print(ex_history)
 for step in range(len(history['acc_0'])):
     wandb.log({k: history[k][step] for k in sorted(history.keys())}, step=step)
+
+for i, step in enumerate(ex_history['step']):
+    wandb.log({'exchange probas': ex_history['proba'][i]}, step=step)
+    # wandb.log({'acceptance ratio': ex_history['accept_ratio'][i]}, step=step)
+
+wandb.log({'num of exchange attempts': len(ex_history['proba'])})
+wandb.log({'num of exchanges': ex_history['swaped'].count(1)})
+
+val_acc = np.array([history[f'val_acc_{i}'] for i in range(config.n_replicas)])
+wandb.log({'best val acc, # of replica, step': [np.round(np.max(val_acc), 3), np.argmax(np.max(val_acc, axis=1)), np.argmax(val_acc) % val_acc.shape[1]]})
+
+
+
+
+
 
 
 model_noswap.compile(optimizer=tf.keras.optimizers.SGD(),
@@ -144,5 +173,5 @@ wandb.log({"train error plot": wandb.Image(train_plt)})
 wandb.log({"test error plot": wandb.Image(test_plt)})
 wandb.log({"train error plot(i)": train_plt})
 wandb.log({"test error plot(i)": test_plt})
-#
-#
+# #
+# #
